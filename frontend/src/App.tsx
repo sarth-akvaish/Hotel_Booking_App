2069d8badf074afc2deccea11ea7e27eaf6af3a8
Sarthak <sarthakvkumar@gmail.com>
2024-01-17 13:57:25 +0530

    HomePage completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
 import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 const App = () => {
   const { isLoggedin } = useAppContext();
@@ -20,7 +21,7 @@ const App = () => {
         path="/"
         element={
           <Layout>
-            <p>Home Page</p>
+            <Home />
           </Layout>
         }
       />
